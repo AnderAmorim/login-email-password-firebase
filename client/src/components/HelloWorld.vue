d170b47767559021908fcc00d406c251fb9cc554
Anderson Amorim <andersonamorim939@gmail.com>
2022-01-04 16:13:02 -0300

    add register

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -3,6 +3,12 @@
     <h1>{{ msg }}</h1>
     <p class="lead mt-2">{{ authStatus }}</p>
     <div class="d-flex my-4 justify-content-center">
+      <input v-model="email" placeholder="Email">
+      <input v-model="password" type="password" placeholder="Password">
+      <button @click="createUser" class="btn btn-outline-danger mx-4">Register ></button>
+
+    </div>
+    <div class="d-flex my-4 justify-content-center">
       <button @click="signIn" class="btn btn-outline-primary mx-4">Sign In ></button>
       <button @click="sendRequest" class="btn btn-outline-success mx-4">Send Request ></button>
       <button @click="signOut" class="btn btn-outline-danger mx-4">Sign Out ></button>
@@ -63,7 +69,7 @@ export default {
     },
     signIn() {
       firebase.auth()
-      .signInWithEmailAndPassword("dummy@gmail.com", "pass123!")
+      .signInWithEmailAndPassword("andersonamorim1641323359792@gmail.com", "12345678")
       .then(() => {
         this.authStatus = 'Authorized'
       }).catch((err) => {
@@ -76,6 +82,11 @@ export default {
       }).catch((err) => {
         this.authStatus = err
       })
+    },
+    createUser(){
+      firebase.auth().createUserWithEmailAndPassword(`andersonamorim${new Date().getTime()}@gmail.com`, '12345678')
+        .then((user) => console.log(user))
+        .catch((error) => console.log(error));
     }
   }
 }
--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import firebase from 'firebase'
 import { FIREBASE_CREDENTIALS } from './envs'
-firebase.initializeApp(FIREBASE_CREDENTIALS)
+firebase.initializeApp({"apiKey":"AIzaSyDaBbl51sQ7kZWlSXmaAcIaHaMNc8Ha4AQ","authDomain":"logins-with-fb-ander.firebaseapp.com","projectId":"logins-with-fb-ander","storageBucket":"logins-with-fb-ander.appspot.com","messagingSenderId":"960575239415","appId":"1:960575239415:web:0f245ff41790406dd84ff2","measurementId":"G-4L8NME9481"})
 
 Vue.config.productionTip = false
 
